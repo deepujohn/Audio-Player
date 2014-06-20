2000e3660f65dec70b7c57b8e255df6a79e1edd7
deepujohn <deepujohn.ideology@gmail.com>
2014-06-20 09:35:08 +0530

    Working

--- Scripts/song.js
+++ Scripts/song.js
@@ -2,7 +2,7 @@ function update(asrc)
 {
 	var audio=document.getElementById('audio');
 	var source=document.getElementById('mp3source');
-	source.src=asrc;
+	source.src="assets/VIP/"+asrc+".mp3";
 	//alert(asrc);	
 	audio.load();
 }
